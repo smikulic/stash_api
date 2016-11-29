3460ab2720e8754f8403e518e5f37af5ddbdedc3
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2016-11-29 23:09:04 +0100

    Add production puma (#16)

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,7 +50,7 @@ set :puma_init_active_record, true  # Change to false when not using ActiveRecor
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/secrets.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/puma.rb'
 
 # Default value for linked_dirs is []
 # append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
