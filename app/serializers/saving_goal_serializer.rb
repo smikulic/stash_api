bf5bd922a4f60d608708d5f6ec286d4d9e80dbca
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2017-08-08 21:37:29 +0200

    Expose created_at timestamp (#25)

--- app/serializers/saving_goal_serializer.rb
+++ app/serializers/saving_goal_serializer.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 # Defines setting serializer
 class SavingGoalSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :description, :deadline, :value
+  attributes :id, :user_id, :description, :deadline, :value, :created_at
 end
