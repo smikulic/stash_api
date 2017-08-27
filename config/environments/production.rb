85e885e80477152007add11beda1a9467b3ce583
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2017-08-27 13:07:46 +0200

    Update secrets and smtp env variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,15 @@ Rails.application.configure do
   config.action_mailer.default_url_options = {
     host: 'http://www.api-staging.scroogevault.co'
   }
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: 'stashbudget@gmail.com',
+    password: ENV["SECRET_SMTP_PASSWORD"],
+    authentication: 'login',
+    enable_starttls_auto: true
+  }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
 
