248e04163b123d88ff4005f9564c0db70c94ded7
Sinisa Mikulic <sinisa.mikulic@gmail.com>
2017-08-29 00:25:18 +0200

    Update settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,6 @@ Rails.application.configure do
   # per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "stash_api_#{Rails.env}"
-  secret_smtp_password = ENV["SECRET_SMTP_PASSWORD"]
 
   config.action_mailer.perform_caching = false
   config.action_mailer.default_url_options = {
@@ -62,7 +61,7 @@ Rails.application.configure do
     port: 587,
     domain: 'gmail.com',
     user_name: 'stashbudget@gmail.com',
-    password: secret_smtp_password,
+    password: ENV["SECRET_SMTP_PASSWORD"],
     authentication: 'login',
     enable_starttls_auto: true
   }
@@ -72,7 +71,7 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery
   # to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
